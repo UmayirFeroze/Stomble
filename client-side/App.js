ac103d73d03a8a92fd7abb8a426b121348c8faa0
Umayir Feroze <umayir10@gmail.com>
2023-07-02 18:30:38 +1000

    Icon Component

--- client-side/App.js
+++ client-side/App.js
@@ -2,11 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, Image, View, SafeAreaView, TouchableHighlight, Button, Platform } from 'react-native';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import MobileScreen from './app/screens/Register/MobileScreen';
+import Screen from './app/components/Screen';
+import Icon from './app/components/Icon';
 
 export default function App () {
   return (
-    <MobileScreen />
+    <Screen>
+
+    </Screen>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({});;;
---
+++ client-side/app/components/AppTextInput.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { TextInput } from 'react-native';
+
+function AppTextInput (props) {
+    return (
+        <TextInput />
+    );
+}
+
+export default AppTextInput;
