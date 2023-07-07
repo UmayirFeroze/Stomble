24c8957d0510b32321ec7d71698c23b2f9fd4d1f
Umayir Feroze <umayir10@gmail.com>
2023-07-07 16:34:01 +1000

    Validation user regsitration
    implementation using Joi

--- server-side/build/controller/register.js
+++ server-side/build/controller/register.js
@@ -1,15 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const database_1 = require("../helpers/database");
+const Joi = require('joi');
 const RegisterUser = (req, res, next) => {
-    const user = {
-        name: req.body.name,
-        phone: req.body.phone,
-        gender: req.body.gender,
-        password: req.body.password,
-        confirmPassword: req.body.confirmPassword
-    };
-    const query = `INSERT INTO users (name, phone, gender, password) VALUES ('${user.name}', '${user.phone}', '${user.gender}', '${user.password}')`;
+    const { error, value } = Validate(req.body);
+    if (error)
+        return res.sendStatus(400).send(error.details[0].message);
+    const query = `INSERT INTO users (name, phone, gender, password) VALUES ('${req.body.name}', '${req.body.phone}', '${req.body.gender}', '${req.body.password}')`;
     (0, database_1.Connect)()
         .then(connection => {
         (0, database_1.Query)(connection, query)
@@ -19,4 +16,15 @@ const RegisterUser = (req, res, next) => {
     })
         .catch(error => { res.status(500).send({ message: error.message, error }); });
 };
+function Validate(user) {
+    const phoneRegex = /^(\+61)[0-9]{9}$/;
+    const schema = {
+        name: Joi.string().min(3).required(),
+        phone: Joi.string().regex(phoneRegex).required(),
+        gender: Joi.string().required(),
+        password: Joi.string().min(8).required(),
+        confirmPassword: Joi.string().valid(Joi.ref('password')).required(),
+    };
+    return Joi.validate(user, schema);
+}
 exports.default = { RegisterUser };
--- server-side/src/controller/register.ts
+++ server-side/src/controller/register.ts
@@ -1,16 +1,14 @@
 import {NextFunction, Request, Response} from 'express';
 import { Connect, Query } from '../helpers/database';
 
-const RegisterUser = (req:Request, res:Response, next:NextFunction) =>{
-    const user = {
-        name: req.body.name, 
-        phone: req.body.phone,
-        gender: req.body.gender, 
-        password: req.body.password, 
-        confirmPassword: req.body.confirmPassword
-    };
+const Joi = require('joi');
 
-    const query = `INSERT INTO users (name, phone, gender, password) VALUES ('${user.name}', '${user.phone}', '${user.gender}', '${user.password}')`;
+const RegisterUser = (req:Request, res:Response, next:NextFunction) =>{
+    
+    const {error, value} = Validate(req.body)
+    if(error) return res.sendStatus(400).send(error.details[0].message);
+
+    const query = `INSERT INTO users (name, phone, gender, password) VALUES ('${req.body.name}', '${req.body.phone}', '${req.body.gender}', '${req.body.password}')`;
 
     Connect()
     .then(connection=>{
@@ -20,6 +18,18 @@ const RegisterUser = (req:Request, res:Response, next:NextFunction) =>{
         .finally( () => connection.end() )
     })
     .catch(error => { res.status(500).send({ message:error.message, error }) })
+}
+
+function Validate(user:any) {
+    const phoneRegex = /^(\+61)[0-9]{9}$/;
+    const schema = {
+        name: Joi.string().min(3).required(),
+        phone: Joi.string().regex(phoneRegex).required(),
+        gender: Joi.string().required(),
+        password: Joi.string().min(8).required(),
+        confirmPassword: Joi.string().valid(Joi.ref('password')).required(),
+    }
+    return Joi.validate(user, schema);
 }
 
 export default { RegisterUser }
